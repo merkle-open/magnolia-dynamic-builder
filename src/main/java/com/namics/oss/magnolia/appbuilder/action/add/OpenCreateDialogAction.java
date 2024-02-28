96ca8f294301bd9b8ca7194a96aa3eaccbcd3cbb
eschleb <bjoern.eschle@merkle.com>
2024-02-28 09:22:08 +0100

    Fix LocaleContext issues on dialogs on root context
     - info.magnolia.ui.dialog.actions.OpenDialogAction initializes the localeContext based on the valueContext. When it is not set (was not set on root context), the localeContext was not being set, which is required for some fields (e.g. multi value)

--- src/main/java/com/namics/oss/magnolia/appbuilder/action/add/OpenCreateDialogAction.java
+++ src/main/java/com/namics/oss/magnolia/appbuilder/action/add/OpenCreateDialogAction.java
@@ -3,7 +3,9 @@ package com.namics.oss.magnolia.appbuilder.action.add;
 import info.magnolia.i18nsystem.I18nizer;
 import info.magnolia.ui.UIComponent;
 import info.magnolia.ui.ValueContext;
+import info.magnolia.ui.api.action.ActionExecutionException;
 import info.magnolia.ui.api.i18n.I18NAuthoringSupport;
+import info.magnolia.ui.datasource.jcr.JcrDatasource;
 import info.magnolia.ui.dialog.DialogDefinition;
 import info.magnolia.ui.dialog.DialogDefinitionRegistry;
 import info.magnolia.ui.dialog.actions.OpenDialogAction;
@@ -13,17 +15,23 @@ import javax.jcr.Node;
 
 public class OpenCreateDialogAction extends OpenDialogAction<Node> {
 	private final OpenCreateDialogActionDefinition definition;
+	private final JcrDatasource jcrDatasource;
+	private final ValueContext<Node> valueContext;
 
 	public OpenCreateDialogAction(
 			final OpenCreateDialogActionDefinition definition,
 			final LocaleContext localeContext,
+			final JcrDatasource jcrDatasource,
 			final ValueContext<Node> valueContext,
 			final UIComponent parentView,
 			final I18NAuthoringSupport<Node> i18NAuthoringSupport,
 			final DialogDefinitionRegistry dialogDefinitionRegistry,
-			final I18nizer i18nizer) {
+			final I18nizer i18nizer
+	) {
 		super(definition, localeContext, valueContext, parentView, i18NAuthoringSupport, dialogDefinitionRegistry, i18nizer);
 		this.definition = definition;
+		this.jcrDatasource = jcrDatasource;
+		this.valueContext = valueContext;
 	}
 
 	@Override
@@ -31,5 +39,17 @@ public class OpenCreateDialogAction extends OpenDialogAction<Node> {
 		final DialogDefinition dialogDefinition = dialogDefinitionRegistry.getProvider(getDefinition().getDialogId()).get();
 		dialogDefinition.getActions().put("commit", definition.getCommitAction()); //we can't generate a new map due to byteBuddy (I18nizer), but since it is a mutable hash map this is fine
 		return i18nizer.decorate(dialogDefinition);
+	}
+
+	@Override
+	public void execute() throws ActionExecutionException {
+		try {
+			if (valueContext.getSingle().isEmpty()) {
+				valueContext.set(jcrDatasource.getRoot());
+			}
+			super.execute();
+		} catch (Exception e) {
+			throw new ActionExecutionException(e);
+		}
 	}
 }
