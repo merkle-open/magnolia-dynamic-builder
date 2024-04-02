f2221c15423ea25ccbc57057c5b3d0bde1933477
eschleb <bjoern.eschle@merkle.com>
2024-04-02 13:09:31 +0200

    Keep guice bean delegation scope

--- src/main/java/com/namics/oss/magnolia/appbuilder/configuration/AppBuilderConfiguration.java
+++ src/main/java/com/namics/oss/magnolia/appbuilder/configuration/AppBuilderConfiguration.java
@@ -4,9 +4,12 @@ import info.magnolia.admincentral.AdmincentralModule;
 import info.magnolia.objectfactory.Components;
 import info.magnolia.ui.api.app.launcherlayout.AppLauncherLayoutManager;
 import info.magnolia.ui.api.app.registry.AppDescriptorRegistry;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +26,19 @@ import org.springframework.stereotype.Service;
 public class AppBuilderConfiguration {
 
 	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public AppLauncherLayoutManager appLauncherLayoutManager() {
 		return Components.getComponent(AppLauncherLayoutManager.class);
 	}
 
 	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public AdmincentralModule admincentralModule() {
 		return Components.getComponent(AdmincentralModule.class);
 	}
 
 	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public AppDescriptorRegistry appDescriptorRegistry() {
 		return Components.getComponent(AppDescriptorRegistry.class);
 	}
