837946d37ec3880247fad9bde14613ec66f69de4
eschleb <bjoern.eschle@merkle.com>
2024-04-17 10:19:11 +0200

    Set export action format to xml

--- src/main/java/com/namics/oss/magnolia/appbuilder/action/importexport/ExportAppActionDefinition.java
+++ src/main/java/com/namics/oss/magnolia/appbuilder/action/importexport/ExportAppActionDefinition.java
@@ -1,19 +1,22 @@
 package com.namics.oss.magnolia.appbuilder.action.importexport;
 
+import info.magnolia.cms.security.Permission;
+import info.magnolia.importexport.command.JcrExportCommand;
+import info.magnolia.ui.api.action.ConfiguredActionDefinition;
+import info.magnolia.ui.contentapp.action.JcrExportActionDefinition;
+
 import com.namics.oss.magnolia.appbuilder.MgnlIcon;
 import com.namics.oss.magnolia.appbuilder.action.AppActionDefinition;
 import com.namics.oss.magnolia.appbuilder.action.AvailabilityDefinitionBuilder;
 import com.namics.oss.magnolia.appbuilder.action.rule.JcrIsNotDeletedRuleDefinition;
 import com.namics.oss.magnolia.appbuilder.action.rule.PermissionRequiredRuleDefinition;
-import info.magnolia.cms.security.Permission;
-import info.magnolia.ui.api.action.ConfiguredActionDefinition;
-import info.magnolia.ui.contentapp.action.JcrExportActionDefinition;
 
 public class ExportAppActionDefinition implements AppActionDefinition {
 
 	@Override
 	public ConfiguredActionDefinition action() {
 		final JcrExportActionDefinition definition = new JcrExportActionDefinition();
+		definition.setFormat(JcrExportCommand.Format.XML);
 		definition.setName("export");
 		definition.setIcon(MgnlIcon.EXPORT);
 		definition.setLabel("actions.export");
