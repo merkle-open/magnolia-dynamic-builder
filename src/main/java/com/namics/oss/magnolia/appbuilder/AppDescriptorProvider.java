25dc9c1e7b8fb8f3e841c3f14b56fe36512175f1
eschleb <bjoern.eschle@merkle.com>
2024-04-22 16:02:58 +0200

    Create new appAppDescriptor on each provider request (instead of eager singleton)

--- src/main/java/com/namics/oss/magnolia/appbuilder/AppDescriptorProvider.java
+++ src/main/java/com/namics/oss/magnolia/appbuilder/AppDescriptorProvider.java
@@ -1,8 +1,5 @@
 package com.namics.oss.magnolia.appbuilder;
 
-import com.namics.oss.magnolia.appbuilder.annotations.AppFactory;
-import com.namics.oss.magnolia.appbuilder.annotations.AppPermissions;
-import com.namics.oss.magnolia.appbuilder.annotations.SubApp;
 import info.magnolia.cms.security.operations.AccessDefinition;
 import info.magnolia.config.registry.DefinitionMetadata;
 import info.magnolia.config.registry.DefinitionProvider;
@@ -14,40 +11,39 @@ import info.magnolia.ui.api.app.SubAppDescriptor;
 import info.magnolia.ui.contentapp.ContentApp;
 import info.magnolia.ui.contentapp.configuration.ContentAppDescriptor;
 import info.magnolia.ui.datasource.DatasourceDefinition;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.aop.support.AopUtils;
 
 import java.lang.annotation.Annotation;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.support.AopUtils;
+
+import com.namics.oss.magnolia.appbuilder.annotations.AppFactory;
+import com.namics.oss.magnolia.appbuilder.annotations.AppPermissions;
+import com.namics.oss.magnolia.appbuilder.annotations.SubApp;
+
 public class AppDescriptorProvider implements DefinitionProvider<AppDescriptor> {
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-	private final ContentAppDescriptor<DatasourceDefinition> appDescriptor;
 	private final DefinitionMetadata metadata;
+	private final Object factoryObject;
+	private final AppFactory annotation;
 
-	public AppDescriptorProvider(final Object appFactory) {
-		metadata = new AppDefinitionMetaDataBuilder(appFactory).build();
-
-		final AppFactory appFactoryAnnotation = AopUtils
-				.getTargetClass(appFactory)
+	public AppDescriptorProvider(final Object factoryObject) {
+		this.factoryObject = factoryObject;
+		this.metadata = new AppDefinitionMetaDataBuilder(factoryObject).build();
+		this.annotation = AopUtils
+				.getTargetClass(factoryObject)
 				.getAnnotation(AppFactory.class);
-
-		appDescriptor = new ContentAppDescriptor<>();
-		appDescriptor.setAppClass(ContentApp.class);
-		appDescriptor.setName(appFactoryAnnotation.name());
-		appDescriptor.setEnabled(appFactoryAnnotation.isEnabled());
-		appDescriptor.setIcon(appFactoryAnnotation.icon());
-		appDescriptor.setTheme(appFactoryAnnotation.theme());
-		appDescriptor.setLabel(appFactoryAnnotation.label());
-		appDescriptor.setI18nBasename(appFactoryAnnotation.i18nBasename());
-		appDescriptor.setSubApps(getSubApps(appFactory, appFactoryAnnotation));
-		getPermissions(appFactory, appFactoryAnnotation).ifPresent(appDescriptor::setPermissions);
 	}
 
 	@Override
@@ -58,11 +54,6 @@ public class AppDescriptorProvider implements DefinitionProvider<AppDescriptor> 
 	@Override
 	public DefinitionMetadata getMetadata() {
 		return metadata;
-	}
-
-	@Override
-	public AppDescriptor get() throws Registry.InvalidDefinitionException {
-		return appDescriptor;
 	}
 
 	@Override
@@ -80,31 +71,45 @@ public class AppDescriptorProvider implements DefinitionProvider<AppDescriptor> 
 		return System.currentTimeMillis();
 	}
 
-	private Map<String, SubAppDescriptor> getSubApps(final Object appFactory, final AppFactory appFactoryAnnotation) {
-		return getDefinitions(appFactory, appFactoryAnnotation, SubApp.class, SubAppDescriptor.class).collect(Collectors.toMap(
+	@Override
+	public AppDescriptor get() throws Registry.InvalidDefinitionException {
+		final ContentAppDescriptor<DatasourceDefinition> appDescriptor = new ContentAppDescriptor<>();
+		appDescriptor.setAppClass(ContentApp.class);
+		appDescriptor.setName(annotation.name());
+		appDescriptor.setEnabled(annotation.isEnabled());
+		appDescriptor.setIcon(annotation.icon());
+		appDescriptor.setTheme(annotation.theme());
+		appDescriptor.setLabel(annotation.label());
+		appDescriptor.setI18nBasename(annotation.i18nBasename());
+		appDescriptor.setSubApps(getSubApps());
+		getPermissions().ifPresent(appDescriptor::setPermissions);
+		return appDescriptor;
+	}
+
+	private Map<String, SubAppDescriptor> getSubApps() {
+		return getDefinitions(SubApp.class, SubAppDescriptor.class).collect(Collectors.toMap(
 				SubAppDescriptor::getName,
 				Function.identity()
 		));
 	}
 
-	private Optional<AccessDefinition> getPermissions(final Object appFactory, final AppFactory appFactoryAnnotation) {
-		return getDefinitions(appFactory, appFactoryAnnotation, AppPermissions.class, AccessDefinition.class).findFirst();
+	private Optional<AccessDefinition> getPermissions() {
+		return getDefinitions(AppPermissions.class, AccessDefinition.class).findFirst();
 	}
 
 	private <T> Stream<T> getDefinitions(
-			final Object appFactory,
-			final AppFactory appFactoryAnnotation,
 			final Class<? extends Annotation> annotation,
-			final Class<T> definitionType) {
-		return Arrays.stream(AopUtils.getTargetClass(appFactory).getDeclaredMethods())
+			final Class<T> definitionType
+	) {
+		return Arrays.stream(AopUtils.getTargetClass(factoryObject).getDeclaredMethods())
 				.filter(method -> method.isAnnotationPresent(annotation))
 				.map(method -> {
 					try {
 						//noinspection unchecked
-						return (T) method.invoke(appFactory);
+						return (T) method.invoke(factoryObject);
 					} catch (IllegalAccessException | InvocationTargetException e) {
-						LOG.error("Could not build " + definitionType.getSimpleName() + ", for app " + appFactoryAnnotation.name(), e);
-						throw new Registry.InvalidDefinitionException(appFactoryAnnotation.id());
+						LOG.error("Could not build " + definitionType.getSimpleName() + ", for app " + this.annotation.name(), e);
+						throw new Registry.InvalidDefinitionException(this.annotation.id());
 					}
 				});
 	}
