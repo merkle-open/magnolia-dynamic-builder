5fd7f770047ef8f1267b4622b96f6f1392211a59
eschleb <bjoern.eschle@merkle.com>
2026-04-01 09:57:02 +0200

    Implement option to disable new warp dialog in confirm delete action

--- magnolia-appbuilder/src/main/java/com/merkle/oss/magnolia/appbuilder/action/edit/ConfirmDeleteAppActionDefinition.java
+++ magnolia-appbuilder/src/main/java/com/merkle/oss/magnolia/appbuilder/action/edit/ConfirmDeleteAppActionDefinition.java
@@ -1,19 +1,39 @@
 package com.merkle.oss.magnolia.appbuilder.action.edit;
 
 import info.magnolia.cms.security.Permission;
+import info.magnolia.context.Context;
+import info.magnolia.i18nsystem.SimpleTranslator;
 import info.magnolia.icons.MagnoliaIcons;
+import info.magnolia.objectfactory.ComponentProvider;
+import info.magnolia.ui.ValueContext;
+import info.magnolia.ui.api.app.SubAppContext;
+import info.magnolia.ui.api.context.UiContext;
+import info.magnolia.ui.contentapp.action.DeleteNodesConfirmationAction;
 import info.magnolia.ui.contentapp.action.DeleteNodesConfirmationActionDefinition;
+import info.magnolia.ui.contentapp.browser.ActionExecutionService;
 import info.magnolia.ui.contentapp.browser.drop.DropConstraintDefinition;
+import info.magnolia.ui.datasource.jcr.JcrDatasourceDefinition;
+import info.magnolia.ui.warp.service.IframeOverlayComponentFactory;
+import info.magnolia.ui.warp.service.IframeOverlayComponentStateUpdater;
+import info.magnolia.warp.component.IframeOverlayComponent;
+
+import java.util.Optional;
+import java.util.Set;
 
 import com.merkle.oss.magnolia.appbuilder.action.AppActionDefinition;
 import com.merkle.oss.magnolia.appbuilder.action.rule.JcrIsNotDeletedRuleDefinition;
 import com.merkle.oss.magnolia.appbuilder.action.rule.PermissionRequiredRuleDefinition;
 import com.merkle.oss.magnolia.definition.builder.availability.AvailabilityDefinitionBuilder;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
+
+import jakarta.inject.Inject;
 
 public class ConfirmDeleteAppActionDefinition implements AppActionDefinition {
 	private final String icon;
+    private boolean warpDialogDisabled;
 
-	public ConfirmDeleteAppActionDefinition() {
+    public ConfirmDeleteAppActionDefinition() {
 		this(MagnoliaIcons.DELETE.getCssClass());
 	}
 
@@ -21,9 +41,17 @@ public class ConfirmDeleteAppActionDefinition implements AppActionDefinition {
 		this.icon = icon;
 	}
 
+	public ConfirmDeleteAppActionDefinition warpDialogDisabled(final boolean warpDialogDisabled) {
+        this.warpDialogDisabled = warpDialogDisabled;
+		return this;
+    }
+
 	@Override
 	public DeleteNodesConfirmationActionDefinition action(final DropConstraintDefinition dropConstraint) {
 		final DeleteNodesConfirmationActionDefinition definition = new DeleteNodesConfirmationActionDefinition();
+		if(warpDialogDisabled) {
+			definition.setImplementationClass(LegacyDeleteNodesConfirmationAction.class);
+		}
 		definition.setName("confirmDelete");
 		definition.setLabel("actions.confirmDeletion");
 		definition.setConfirmationHeader("actions.confirmDeletion.confirmationHeader");
@@ -43,5 +71,32 @@ public class ConfirmDeleteAppActionDefinition implements AppActionDefinition {
 	@Override
 	public boolean multiple() {
 		return true;
+	}
+
+	public static class LegacyDeleteNodesConfirmationAction<D extends DeleteNodesConfirmationActionDefinition> extends DeleteNodesConfirmationAction<D> {
+		@Inject
+		public LegacyDeleteNodesConfirmationAction(
+				final D definition,
+				final UiContext uiContext,
+				final ActionExecutionService actionExecutionService,
+				final SimpleTranslator i18n,
+				final ValueContext valueContext,
+				final JcrDatasourceDefinition datasourceDefinition,
+				final IgnoreIframeOverlayComponentFactory iframeOverlayComponentFactory,
+				final SubAppContext subAppContext
+		) {
+			super(definition, uiContext, actionExecutionService, i18n, valueContext, datasourceDefinition, iframeOverlayComponentFactory, subAppContext);
+		}
+
+		public static class IgnoreIframeOverlayComponentFactory<T> extends IframeOverlayComponentFactory<T> {
+			@Inject
+			public IgnoreIframeOverlayComponentFactory(final Set<IframeOverlayComponentStateUpdater> set, ComponentProvider componentProvider) {
+				super(set, componentProvider);
+			}
+			@Override
+			public Optional<Component> initializeOverlayComponent(final ComponentContainer componentContainer, final ValueContext<T> valueContext, final IframeOverlayComponent.IframeInfo iframeInfo, final Context context) {
+				return Optional.empty();
+			}
+		}
 	}
 }
