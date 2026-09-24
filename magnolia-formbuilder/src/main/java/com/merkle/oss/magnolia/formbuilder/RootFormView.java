cd11a7b44478b179b2587c3dcd6c7eacd17f643b
eschleb <bjoern.eschle@merkle.com>
2026-09-24 14:29:05 +0200

    Fix possible nullPointer due to race condition

--- magnolia-formbuilder/src/main/java/com/merkle/oss/magnolia/formbuilder/RootFormView.java
+++ magnolia-formbuilder/src/main/java/com/merkle/oss/magnolia/formbuilder/RootFormView.java
@@ -7,6 +7,7 @@ import info.magnolia.ui.editor.LocaleContext;
 import info.magnolia.ui.field.FieldDefinition;
 
 import java.util.Map;
+import java.util.Optional;
 
 import com.vaadin.ui.Component;
 
@@ -19,7 +20,7 @@ public class RootFormView<T> extends FormView<T> {
     public RootFormView(final RootFormView.Definition<T> formDefinition, final LocaleContext localeContext) {
         super(formDefinition, localeContext);
         this.formDefinition = formDefinition;
-        updateViewType(asVaadinComponent());
+        Optional.ofNullable(asVaadinComponent()).ifPresent(this::updateViewType);
     }
 
     @Override
